a3bbc3db41e3e496718904a016af6a775fcc75f1
tommy-vaux <tommyvaux@icloud.com>
2019-12-10 17:30:18 +1100

    Basic AI movement & building detects multiple enemies
    Also in the process of creating the game data assets

---
+++ Assets/Scripts/enemys/enemyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemyController : MonoBehaviour
+{
+
+    public NavMeshAgent agent;
+
+    public enemy data;
+
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = target.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
